30df37b266818bd27583405b8f2eaa3758cb5c93
Steven Shyun <stevenshuhyow@yahoo.com>
2015-02-26 01:06:22 -0800

    Not sure why I have an undefined function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,18 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
-};
+
+
+
+var getElementsByClassName = function(className){
+	var blankArray=[];
+	var nodes =document.body.childNodes; 
+	console.log(nodes)
+	nodes.forEach(function(element){
+		console.log(element);
+		if(element.nodeType ===1 && element.classList.contains(className)){
+			blankArray.push(element);
+		}
+	})
+	return blankArray;
+}
